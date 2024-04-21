655421a4dd0cacb7ac0c06575a2169bd32f81e01
Nikita Novikov <144739976+ustalyj-putnik@users.noreply.github.com>
2024-04-21 20:40:34 +0300

    Add files via upload

---
+++ lab_04/start.sh
@@ -0,0 +1,11 @@
+export DB_HOST=lab_04_postgres
+export DB_PORT=5432
+export DB_LOGIN=root
+export DB_PASSWORD=root
+export DB_DATABASE=archdb
+export MONGO_HOST=lab04_mongo
+export MONGO_PORT=27017
+export MONGO_DATABASE=arch
+export JWT_KEY=0123456789ABCDEF0123456789ABCDEF
+
+/app/build/lab_04
